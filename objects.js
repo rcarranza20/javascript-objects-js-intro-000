91931de496f0ee7923fc2c8800d6ed027a59e65a
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-19 22:55:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object ({keys: '0'});
  
 function updatePlaylist (playlist, artistName, songTitle){
-  playlist.
+  playlist.artistName = "songTitle";
   return updatePlaylist
 }
